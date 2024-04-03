e66f71d22542ad2faddcf37354c63ec3a9cc1382
lamgnaoh <luonglamhoang2000@gmail.com>
2024-04-03 16:35:53 +0700

    first symfony project , add twig engine template and php-cs-fixer library

---
+++ src/Controller/MainController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        $numberVisitor = 100;
+
+        return $this->render('main/homepage.html.twig', [
+            'numberVisitor' => $numberVisitor,
+        ]);
+    }
+}

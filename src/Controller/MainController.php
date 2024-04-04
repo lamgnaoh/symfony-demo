a87cbf84977124c4b9fae7a969bc6020d6fe48ef
lamgnaoh <luonglamhoang2000@gmail.com>
2024-04-04 10:00:59 +0700

    create a repository which is service to fetch all product

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Controller\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,12 +10,13 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController extends AbstractController
 {
     #[Route('/')]
-    public function homepage(): Response
+    public function homepage(ProductRepository $repository): Response
     {
-        $numberVisitor = 100;
+        $products = $repository->findAll();
+        $numsProduct = count($products);
 
         return $this->render('main/homepage.html.twig', [
-            'numberVisitor' => $numberVisitor,
+            '$numsProduct' => $numsProduct,
         ]);
     }
 }
--- src/Controller/ProductApiController.php
+++ src/Controller/ProductApiController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Controller\Model\Product;
+use App\Controller\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,42 +10,9 @@ use Symfony\Component\Routing\Attribute\Route;
 class ProductApiController extends AbstractController
 {
     #[Route('api/v1/products')]
-    public function getCollection(): Response
+    public function getCollection(ProductRepository $repository): Response
     {
-        $products = [
-            new Product(
-                1,
-                'Iphone 13',
-                400,
-                true,
-                '2024-04-03T00:00:00+00:00',
-                '2024-04-03T70:00:00+00:00'
-            ),
-            new Product(
-                2,
-                'Ipad gen 9',
-                200,
-                false,
-                '2024-04-02T00:00:00+00:00',
-                '2024-04-03T70:00:00+00:00'
-            ),
-            new Product(
-                3,
-                'Macbook Air Pro',
-                1000,
-                true,
-                '2024-04-01T00:00:00+00:00',
-                '2024-04-03T00:00:00+00:00'
-            ),
-            new Product(
-                4,
-                'Airpods',
-                70,
-                true,
-                '2024-04-03T00:00:00+00:00',
-                '2024-04-03T70:00:00+00:00'
-            ),
-        ];
+        $products = $repository->findAll();
 
         return $this->json($products);
     }
